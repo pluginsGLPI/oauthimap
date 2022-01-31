83d742ca940bb8aa03801c3b961a79dd50063951
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 15:42:17 +0100

    Fix licence headers

--- inc/provider/providerinterface.class.php
+++ inc/provider/providerinterface.class.php
@@ -1,27 +1,31 @@
 <?php
+
 /**
- -------------------------------------------------------------------------
- oauthimap plugin for GLPI
- Copyright (C) 2018-2020 by the oauthimap Development Team.
- -------------------------------------------------------------------------
-
- LICENSE
-
- This file is part of oauthimap.
-
- oauthimap is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- oauthimap is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with oauthimap. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
+ * OauthIMAP plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of OauthIMAP.
+ *
+ * OauthIMAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OauthIMAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OauthIMAP. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2020-2022 by OauthIMAP plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/oauthimap
+ * -------------------------------------------------------------------------
  */
 
 namespace GlpiPlugin\Oauthimap\Provider;
