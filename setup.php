ef8e7552815333331ac4546a154bacc7a0dc30b9
Cédric Anne <cedric.anne@gmail.com>
2022-10-28 10:33:55 +0200

    1.3.4

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_OAUTHIMAP_VERSION', '1.3.3');
+define('PLUGIN_OAUTHIMAP_VERSION', '1.3.4');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_OAUTHIMAP_MIN_GLPI', '9.5');
