e259d47ea5be4734f54fd90d1f30a382e953ee4d
Romain B. <8530352+Rom1-B@users.noreply.github.com>
2025-06-16 16:28:22 +0200

    Update HEADER (#87)

--- inc/authorization.class.php
+++ inc/authorization.class.php
@@ -2,29 +2,29 @@
 
 /**
  * -------------------------------------------------------------------------
- * OauthIMAP plugin for GLPI
+ * oauthimap plugin for GLPI
  * -------------------------------------------------------------------------
  *
  * LICENSE
  *
- * This file is part of OauthIMAP.
+ * This file is part of oauthimap plugin.
  *
- * OauthIMAP is free software; you can redistribute it and/or modify
+ * This plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OauthIMAP is distributed in the hope that it will be useful,
+ * This plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OauthIMAP. If not, see <http://www.gnu.org/licenses/>.
+ * along with this plugin. If not, see <https://www.gnu.org/licenses/>.
  * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2020-2022 by OauthIMAP plugin team.
- * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/pluginsGLPI/oauthimap
+ * @copyright Copyright (C) 2020-2025 by Teclib'
+ * @license   GPLv3+ https://www.gnu.org/licenses/gpl-3.0.fr.html
+ * @link      https://services.glpi-network.com
  * -------------------------------------------------------------------------
  */
 
